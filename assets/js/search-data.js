c6d54518678c3b2ade23fe0a96ff8a5b9ceced4e
ghadeer294 <g.almusaddar19@gmail.com>
2026-03-22 16:34:46 +0000

    Deploying to gh-pages from @ ghadeer294/ghadeer294.github.io@7a904dfb234c23105ecead29e8c3ae619fc9771b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -444,39 +444,18 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%67%61%6C%6D%75%73%61%31[%61%74]%62%69%6E%67%68%61%6D%74%6F%6E[%64%6F%74]%65%64%75", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=-zHU7wkAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
